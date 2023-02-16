a19288f5c1ce8d8e7d3b50e9d82b8bab8967d30a
yogiithaa <118190460+yogiithaa@users.noreply.github.com>
2023-02-16 10:11:41 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins-PES2UG20CS399\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
